5f95bd0dc51cf6d80aa040b18f33f1f329f8dc9b
cochese311 <zrshovar@gmail.com>
2021-12-01 08:24:04 -0500

    Adding madlib project

---
+++ madlib.py
@@ -0,0 +1,10 @@
+noun_1 = input("Choose a noun and press enter: ")
+verb_1 = input("Choose a verb and press enter: ")
+adjective_1 = input("Choose an adjective and press enter: ")
+adverb_1 = input("Choose an adverb and press enter: ")
+noun_2 = input("Choose a new noun and press enter: ")
+adjective_2 = input("Choose an new adjective and press enter: ")
+
+story = ("The " + noun_1 + " tried to " + verb_1 + " and saw a " + adjective_1  +  noun_2 + " and found a " + noun_2 + " that looked really " + adjective_2)
+
+print(story)
